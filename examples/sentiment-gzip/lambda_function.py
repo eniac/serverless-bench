295f148adb8f7909d644b3f5447098c6176bbea0
Xuting Liu <xutingl@seas.upenn.edu>
2025-02-06 13:57:23 -0500

    Add lambda_function for AWS lambda applications.

---
+++ examples/sentiment-gzip/lambda_function.py
@@ -0,0 +1,47 @@
+import time
+IMPORT_START_TIME = time.time()
+import warnings
+import gzip
+import pickle
+IMPORT_END_TIME = time.time()
+import_time = IMPORT_END_TIME - IMPORT_START_TIME
+# Suppress specific scikit-learn warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="sklearn")
+
+CLASSES = {
+    0: "negative",
+    4: "positive"
+}
+
+MODEL_FILE = 'model.dat.gz'
+with gzip.open(MODEL_FILE, 'rb') as f:
+    MODEL = pickle.load(f, encoding='latin1')
+
+# pylint: disable=unused-argument
+def lambda_handler(event, context=None):
+    """
+        Validate parameters and call the recommendation engine
+        @event: API Gateway's POST body;
+        @context: LambdaContext instance;
+    """
+
+    # input validation
+    assert event, "AWS Lambda event parameter not provided"
+    text = event.get("text")  # query text
+    assert isinstance(text, str)
+
+    # call predicting function
+    prediction = predict(text)
+    return {"import_time": IMPORT_END_TIME - IMPORT_START_TIME, "prediction": prediction}
+
+
+def predict(text):
+    """
+        Predict the sentiment of a string
+        @text: string - the string to be analyzed
+    """
+
+    x_vector = MODEL.vectorizer.transform([text])
+    y_predicted = MODEL.predict(x_vector)
+
+    return CLASSES.get(y_predicted[0])
